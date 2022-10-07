1155c7830e91354fca1178ecf79e5145e40bd9e7
dhaval-rao <dhavaldashadia.rao@gmail.com>
2022-10-07 16:06:48 +0530

    removed api

--- app.py
+++ app.py
@@ -4,28 +4,28 @@ from functions import Predictor
 
 app = Flask(__name__)
 
-pred = Predictor('latest_model.h5')
+# pred = Predictor('latest_model.h5')
 
 @app.route('/')
 def home():
     return "hello world"
 
 
-@app.route('/translate', methods=['POST'])
-def translate():
-    video_path = request.form.get('video') #video path
-    # result = {'pictures': pictures}
-
-    res = json.loads(requests.get(video_path).content.decode("utf-8"))
-    video_path += "?alt=media&token=" + res["downloadTokens"]
-
-    text = pred.predict(video_path)
-
-    result = {
-        'word': text
-    }
-
-    return jsonify(result)
+# @app.route('/translate', methods=['POST'])
+# def translate():
+#     video_path = request.form.get('video') #video path
+#     # result = {'pictures': pictures}
+#
+#     # res = json.loads(requests.get(video_path).content.decode("utf-8"))
+#     # video_path += "?alt=media&token=" + res["downloadTokens"]
+#
+#     text = pred.predict(video_path)
+#
+#     result = {
+#         'word': text
+#     }
+#
+#     return jsonify(result)
 
 
 if __name__ == '__main__':
